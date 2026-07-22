acf093b6972203d90f03071109388cc104fb7cf6
OJOkhiria <ojeifookhiria@gmail.com>
2026-07-21 19:25:04 -0500

    Cleanup & Organization
    Deleted some obsolete files and organized a few into folders.

--- Levels/area_2d.gd
+++
@@ -1,8 +0,0 @@
-extends Area2D
-
-var player_node: CharacterBody2D = null
-var player_body_in: bool = false
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
